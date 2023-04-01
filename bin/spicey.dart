d073dacd78353480b7c2173207ee437de5aca671
Rahyulty <ramlalajadhi@gmail.com>
2023-04-01 23:28:24 +0000

    init

---
+++ bin/spicey.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_commands/nyxx_commands.dart';
+import '../lib/commands.dart';
+
+void main() async {
+  final token = Platform.environment['discordToken']!;
+  final client = NyxxFactory.createNyxxWebsocket(token, GatewayIntents.all);
+  final commands = CommandsPlugin(
+    prefix: mentionOr((_) => "?"),
+    options: CommandsOptions(logErrors: false)
+);
+
+  commands
+    ..addCommand(avatar)
+    ..addCommand(ping);
+
+  client
+    ..eventsWs.onReady.listen((event) {
+      var shardManager = client.shardManager;
+      
+      shardManager.setPresence(PresenceBuilder.of(
+        activity: ActivityBuilder('to my heart', ActivityType.listening)));
+    })
+    ..eventsWs.onMessageDelete.listen((event) {
+      // logic
+    });
+  
+    
+}
---
+++ lib/commands.dart
@@ -0,0 +1,9 @@
+//bot commands
+
+library botcommands;
+
+export 'src/Commands/ping.dart';
+export 'src/Commands/avatar.dart';
+
+
+
