9d95e7c341cbfaa4ddf882604a709eb1213b721a
Rahyulty <ramlalajadhi@gmail.com>
2023-04-10 17:05:17 +0000

    Clog in console

--- bin/spicey.dart
+++ bin/spicey.dart
@@ -31,7 +31,6 @@ void main() async {
     ..registerPlugin(Logging())
     ..registerPlugin(CliIntegration())
     ..registerPlugin(IgnoreExceptions())
-    ..registerPlugin(Logging(logLevel: Level.ALL))
     ..registerPlugin(commands);
 
   await client.connect();
